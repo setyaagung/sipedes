5276ada36e5d61416e37668cb3b95cd766a5f6ff
Muhammad Agung Setya Budi <setyaagung74@gmail.com>
2021-06-23 16:35:11 +0700

    print pindah pergi

--- resources/views/layouts/back-main.blade.php
+++ resources/views/layouts/back-main.blade.php
@@ -18,6 +18,7 @@
     <!-- Custom styles for this template-->
     <link href="{{ asset('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
     <!-- Custom styles for this page -->
+    <link rel="icon" type="image/png" href="{{ asset('img/pemkot.png')}}">
     <link href="{{ asset('assets/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
     <!-- Select2 -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/select2/css/select2.min.css')}}">
